31c6fc62e0c4db73df9a34795ba64df883531058
BPastorPortugal <brendapastorportugal@gmail.com>
2025-06-18 05:54:03 -0500

    avance de la administracion (#24)

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,30 +1,30 @@
-// src/app/(admin)/dashboard/page.tsx
 import React from "react";
-import AdminNavbar from "@/components/navbar/AdminNavbar";
+import TopFiveCard from "./Components/TopFiveCard";
+import Breadcrumb from "@/app/(admin)/dashboard/Components/Breadcrumb";
+import DashboardContent from "@/app/(admin)/dashboard/Components/DashboardContent";
 
 const DashboardPage = () => {
   return (
-    <div className="min-h-screen bg-[#F2F2F0]">
-      <AdminNavbar />
-      <main className="p-6">
-        <h1 className="text-2xl font-bold text-[#2A5559] mb-4">
-          Bienvenido/a al Panel de Administración
-        </h1>
-        {/* Aquí luego irán las cards resumen */}
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          <div className="bg-white p-4 rounded-2xl shadow text-[#593723]">
-            <h2 className="text-lg font-semibold">Total Perritos</h2>
-            <p className="mt-2 text-3xl">12</p>
-          </div>
-          <div className="bg-white p-4 rounded-2xl shadow text-[#593723]">
-            <h2 className="text-lg font-semibold">Adoptados</h2>
-            <p className="mt-2 text-3xl">7</p>
-          </div>
-          <div className="bg-white p-4 rounded-2xl shadow text-[#593723]">
-            <h2 className="text-lg font-semibold">Pendientes</h2>
-            <p className="mt-2 text-3xl">5</p>
-          </div>
-        </div>
+    <div className="min-h-screen bg-[#F2F2F0] flex flex-col">
+      {/* Navbar superior */}
+
+      {/* Contenido principal con grid */}
+      <main className="grid grid-cols-1 lg:grid-cols-[1fr_300px] gap-6 p-6">
+        {/* Contenido del dashboard */}
+        <section className="w-full">
+
+          <h1 className="text-2xl font-bold text-[#2A5559] mb-4 text-center">
+            Bienvenido/a al Panel de Administración
+          </h1>
+          <Breadcrumb />
+          <DashboardContent />
+
+        </section>
+
+        {/* Panel lateral con el ranking */}
+        <aside className="w-full">
+          <TopFiveCard />
+        </aside>
       </main>
     </div>
   );
