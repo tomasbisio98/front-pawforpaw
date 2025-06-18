42ec43dda0164b54b0753ab09ecfc5988768af39
karen_ximena <158591103+kaxime@users.noreply.github.com>
2025-06-18 09:49:23 -0500

    arreglo de login y register y estilos en nav y footer (#25)

---
+++ src/app/(single-page)/authpag/components/GoogleAuth.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const GoogleAuth = () => {
+    return (
+        <div>
+            <p>Inicia sesion con</p>
+            <Link href = "#" ></Link>
+        </div>
+    )
+}
+
+export default GoogleAuth;
--- src/app/(single-page)/authpag/page.tsx
+++ src/app/(single-page)/authpag/page.tsx
@@ -4,37 +4,40 @@ import Link from "next/link";
 import LoginForm from "./components/LoginForm"
 import RegisterForm from "./components/RegisterForm";
 import { FiArrowLeft } from "react-icons/fi";
+import { routes } from "@/routes";
 
 const AuthPage = () => {
     return (
-      <main className="min-h-screen bg-gray-200 p-4 pt-11 flex flex-col items-center">
-  {/* Link a la derecha */}
+      <main className="min-h-screen bg-blancoSuave p-4 pt-11 flex flex-col items-center">
+  
   <div className="w-full max-w-6xl flex items-center justify-between mb-8">
 
     <div className="flex items-center">
-        <img src="/paw.png" alt="Logo" className="h-12 w-auto rounded-md " />
+        <Link href={routes.inicio}>
+            <img src="/paw.png" alt="Logo" className="h-12 w-auto rounded-full " />
+        </Link>
     </div>
 
     <Link
-      href="#"
-      className="text-slate-800 font-medium bg-emerald-300 rounded-md px-4 py-2 flex items-center gap-2"
+      href={routes.inicio}
+      className="text-green-900 font-medium bg-verdeSuave rounded-md px-4 py-2 flex items-center gap-2"
     ><FiArrowLeft /> Regresar al Inicio
     </Link>
   </div>
 
   {/* Contenedor central de los formularios */}
-  <div className="bg-white p-8 rounded-2xl shadow-lg w-full max-w-6xl flex flex-col md:flex-row items-start justify-center gap-12">
+  <div className="bg-white p-8 rounded-2xl shadow-lg w-[80%] max-w-6xl flex flex-col md:flex-row items-start justify-center ">
     {/* Login Form */}
-    <div className="flex-1 min-w-[280px]">
-      <h2 className="text-3xl font-bold text-gray-800 text-center mb-6">
+    <div className="flex-1 flex flex-col items-center">
+      <h2 className="text-3xl font-bold text-green-950 text-center mb-6">
         Iniciar Sesión
       </h2>
       <LoginForm />
     </div>
-
+     
     {/* Register Form */}
-    <div className="flex-1 min-w-[280px]">
-      <h2 className="text-3xl font-bold text-gray-800 text-center mb-6">
+    <div className="flex-1">
+      <h2 className="text-3xl font-bold text-green-950 text-center mb-6">
         Crear Cuenta
       </h2>
       <RegisterForm />
