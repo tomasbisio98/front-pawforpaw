5399311b87ec45ac05d25bc1892a2f3a5e563045
Tomás Bisio <tomibisio1@gmail.com>
2025-06-25 01:15:35 -0300

    Newsletter conexion (#41)
    * API connexion to subscribe users
    
    * finishing history page with more photos + adding NEXT_PUBLIC_API_URL
    
    * adding filters to see dogs as a user
    
    * addding perritoPage working
    
    * prevent newsletter unexpected view
    
    * minor fixes to show all dogs + one dog specific

--- src/app/(views)/perritos/page.tsx
+++ src/app/(views)/perritos/page.tsx
@@ -1,23 +1,74 @@
-import DogsList from "@/components/dogsList/DogsList";
+'use client';
 
-import { getDogs } from "@/service/dogs";
+import { useEffect, useState } from 'react';
+import DogsList from '@/components/dogsList/DogsList';
+import { getDogsFilter } from '@/service/dogs';
+import FiltroPerritos from '@/components/filters/filtroPerritos'; // lo vas a crear
+import { IDogs } from '@/interface/IDogs';
 
-const getData = async () => {
-  const dogs = await getDogs();
+const Perritos = () => {
+  const [filters, setFilters] = useState({
+    name: '',
+    gender: '',
+    city: '',
+    page: 1,
+    limit: 9,
+  });
 
-  return {
-    dogs,
-  };
-};
+  const [dogs, setDogs] = useState<IDogs[]>([]);
+  const [total, setTotal] = useState(0);
 
-const Perritos = async () => {
-  const { dogs } = await getData();
+  useEffect(() => {
+    const fetchDogs = async () => {
+      const { data, total } = await getDogsFilter(filters);
+      setDogs(data);
+      setTotal(total);
+    };
+
+    fetchDogs();
+  }, [filters]);
+
   return (
     <main className="px-4 py-8 space-y-8 bg-blancoSuave">
       <h2 className="text-2xl text-center font-bold">Perritos</h2>
+
+      <FiltroPerritos filters={filters} setFilters={setFilters} />
+
       <div className="flex justify-center">
-       <DogsList list={dogs}/>
+        <DogsList list={dogs} />
       </div>
+
+     <div className="flex justify-center gap-4 items-center text-sm text-gray-600">
+  <button
+    onClick={() =>
+      setFilters((prev) => ({
+        ...prev,
+        page: Math.max(1, prev.page - 1),
+      }))
+    }
+    disabled={filters.page === 1}
+    className="px-3 py-1 border rounded disabled:opacity-40"
+  >
+    ← Anterior
+  </button>
+
+  <span>
+    Página {filters.page} de {Math.ceil(total / filters.limit) || 1}
+  </span>
+
+  <button
+    onClick={() =>
+      setFilters((prev) => ({
+        ...prev,
+        page: prev.page + 1,
+      }))
+    }
+    disabled={filters.page >= Math.ceil(total / filters.limit)}
+    className="px-3 py-1 border rounded disabled:opacity-40"
+  >
+    Siguiente →
+  </button>
+</div>
     </main>
   );
 };
