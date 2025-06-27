729c783f9494529a36e0e8314dd0097d88650424
NicolasGC5 <nicolasxd48@gmail.com>
2025-06-26 19:42:16 -0500

    Nico front2 (#50)
    * Crud de perritos (admin) terminado
    
    * Eliminado archivo innecesario: archivo.txt
    
    * Productos de perritos andando fase 1
    
    * Creación de segunda entidad de usuario para no pisar la entidad de Karen
    
    * Ajuste total luego de cambio de interfaz
    
    * Productos de perritos Done
    
    * Cambios de Juan
    
    * Cambios de Juan
    
    ---------
    
    Co-authored-by: nicoguaro20 <jungarciagu@unal.edu.co>

--- src/app/(admin)/dashboard/Components/UserTable.tsx
+++ src/app/(admin)/dashboard/Components/UserTable.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { getUser } from "@/service/user";
-import { IUser } from "@/interface/IUsers";
+import { getUser2 } from "@/service/user";
+import { IUsers2 } from "@/interface/IUsers2";
 import { updateStatusUsuario } from "@/service/user";
 
 const UserTable = () => {
-  const [usuarios, setUsuarios] = useState<IUser[]>([]);
+  const [usuarios, setUsuarios] = useState<IUsers2[]>([]);
   const [paginaActual, setPaginaActual] = useState(1);
-  const itemsPorPagina = 15;
+  const itemsPorPagina = 20;
 
   useEffect(() => {
     const fetchUsuarios = async () => {
       try {
-        const data = await getUser();
+        const data = await getUser2();
         setUsuarios(data);
       } catch (error) {
         console.error("Error al obtener usuarios:", error);
@@ -22,7 +22,7 @@ const UserTable = () => {
 
     fetchUsuarios();
   }, []);
-  
+
 
   const totalPaginas = Math.ceil(usuarios.length / itemsPorPagina);
   const usuariosPaginados = usuarios.slice(
@@ -30,24 +30,24 @@ const UserTable = () => {
     paginaActual * itemsPorPagina
   );
 
-const toggleEstado = async (id: string, currentStatus: boolean) => {
-  try {
-    const newStatus = !currentStatus;
+  const toggleEstado = async (id: string, currentStatus: boolean) => {
+    try {
+      const newStatus = !currentStatus;
 
-    // 1. Actualiza en backend
-    await updateStatusUsuario(id, newStatus);
+      // 1. Actualiza en backend
+      await updateStatusUsuario(id, newStatus);
 
-    // 2. Refleja el cambio en frontend
-    setUsuarios((prev) =>
-      prev.map((user) =>
-        user.id === id ? { ...user, status: newStatus } : user
-      )
-    );
-  } catch (error) {
-    console.error("Error al actualizar estado:", error);
-    alert("No se pudo actualizar el estado del usuario.");
-  }
-};
+      // 2. Refleja el cambio en frontend
+      setUsuarios((prev) =>
+        prev.map((user) =>
+          user.id === id ? { ...user, status: newStatus } : user
+        )
+      );
+    } catch (error) {
+      console.error("Error al actualizar estado:", error);
+      alert("No se pudo actualizar el estado del usuario.");
+    }
+  };
 
   return (
     <div className="overflow-x-auto">
@@ -63,21 +63,21 @@ const toggleEstado = async (id: string, currentStatus: boolean) => {
         </thead>
         <tbody>
           {usuariosPaginados.map((u) => (
-            <tr key={u.id} className="border-t hover:bg-gray-100">
+            <tr key={u.id} className="border-t hover:bg-gray-100 text-gray-800">
               <td className="p-2">{u.name}</td>
               <td>{u.email}</td>
               <td>{u.phone}</td>
               <td>
-              <label className="relative inline-flex items-center cursor-pointer">
-              <input
-                type="checkbox"
-                checked={u.status}
-                onChange={() => toggleEstado(u.id, u.status)}
-                className="sr-only peer"
-              />
-                <div className="w-11 h-6 bg-gray-300 peer-checked:bg-green-500 rounded-full peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-500 transition-colors duration-300" />
-                <span className="absolute left-0.5 top-0.5 w-5 h-5 bg-white rounded-full shadow transform transition-transform duration-300 peer-checked:translate-x-5"></span>
-              </label>
+                <label className="relative inline-flex items-center cursor-pointer">
+                  <input
+                    type="checkbox"
+                    checked={u.status}
+                    onChange={() => toggleEstado(u.id, u.status)}
+                    className="sr-only peer"
+                  />
+                  <div className="w-11 h-6 bg-gray-300 peer-checked:bg-green-500 rounded-full peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-500 transition-colors duration-300" />
+                  <span className="absolute left-0.5 top-0.5 w-5 h-5 bg-white rounded-full shadow transform transition-transform duration-300 peer-checked:translate-x-5"></span>
+                </label>
               </td>
               <td>S/ {u.montoDonado?.toFixed(2)}</td>
             </tr>
